f78a08ac8de7c6c847a530a5a0e98c9159a5d749
shawag <89336833+shawag@users.noreply.github.com>
2025-05-07 21:27:45 +0800

    二次打包interface

---
+++ source/pkt_if_pack.sv
@@ -0,0 +1,15 @@
+`include "pkt_if.sv"
+
+`ifdef PKT_INTERFACE_SV
+define PKT_INTERFACE_SV
+interface pkt_if_pack(
+    input logic clk ,
+    input logic rst_n 
+);
+
+    pkt_if  pkt_in_bus (clk, rst_n);
+    pkt_if  pkt_out_bus (clk, rst_n);
+endinterface : pkt_if_pack
+
+
+`endif
